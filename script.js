83efdb19582c9467d6b9ab60b1cd0e4ffe5bf4c3
Orincy Whyte <orincywhyte@gmail.com>
2022-03-22 16:15:40 -0400

    commit

--- script.js
+++ script.js
@@ -4,18 +4,19 @@ const cardName = document.querySelector(".name");
 const cardSpecies = document.querySelector(".species");
 const cardCont = document.querySelector(".card_container");
 
-
-// fetch("https://rickandmortyapi.com/api/character")
-//   .then((res) => res.json())
-//   .then((data) => rickAndMorty(data.results));
-
-// let rickAndMorty = (characters) => {
-//   characters.forEach((character) => {
-//     cardImage.src = `${character.image}`;
-//     cardName.textContent = `Name: ${character.name}`;
-//     cardSpecies.textContent = `Species: ${character.species}`;
-//   });
-// };
+// third option
+//api call
+fetch("https://rickandmortyapi.com/api/character")
+  .then((res) => res.json())
+  .then((data) => rickAndMorty(data.results));
+//dom
+let rickAndMorty = (characters) => {
+  characters.forEach((character) => {
+    cardImage.src = `${character.image}`;
+    cardName.textContent = `Name: ${character.name}`;
+    cardSpecies.textContent = `Species: ${character.species}`;
+  });
+};
 
 // // 2nd Option
 // fetch("https://rickandmortyapi.com/api/character")
@@ -28,20 +29,20 @@ const cardCont = document.querySelector(".card_container");
 //     })
 //   );
 
-// 1st option
-let cards = [];
-let rickAndMorty = () => {
-  return fetch("https://rickandmortyapi.com/api/character")
-    .then((response) => response.json())
-    .then((data) => data.results.map((result) => cards.push(result)));
-};
+// // 1st option
+// let cards = [];
+// let rickAndMorty = () => {
+//   return fetch("https://rickandmortyapi.com/api/character")
+//     .then((response) => response.json())
+//     .then((data) => data.results.map((result) => cards.push(result)));
+// };
 
-let apiData = cards.map((character) => {
-  cardCont.innerHTML = 
-  `Name: ${character.name}  
-  Species: ${character.species}  
-  Status: ${character.status} 
-  Type: ${character.type} <br>`;
-});
-rickAndMorty();
-console.log(cards);
+// let apiData = cards.map((character) => {
+//   cardCont.innerHTML =
+//   `Name: ${character.name}
+//   Species: ${character.species}
+//   Status: ${character.status}
+//   Type: ${character.type} <br>`;
+// });
+// rickAndMorty();
+// console.log(cards);
